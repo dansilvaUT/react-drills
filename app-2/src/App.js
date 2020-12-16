6cb425f38738f2d438d77083b54f4338605ee903
dansilvaUT <dan13silv@gmail.com>
2020-12-16 13:21:10 -0800

    Finished app2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: ['Java', 'HTML', 'CSS', 'JavaScript', 'MySQL', 'Git', 'Node.js', 'React']
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Languages</h1>
+        {this.state.items.map((element, index) => {
+          return <h4 key={index}>{element}</h4>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;

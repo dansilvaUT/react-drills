1fe50264c7031853bbb68be85a1d88feae9edcf3
dansilvaUT <dan13silv@gmail.com>
2020-12-21 20:00:48 -0800

    practice

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { Component } from 'react';
 import './App.css';
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      userInput: ''
+      input: ''
     }
   }
 
-  handleInputChange(value) {
-    this.setState({ userInput: value });
+  handleChange(value) {
+    this.setState({ input: value });
   }
-
   render() {
+    console.log(this.state.input)
     return (
       <div className="App">
-        <input onChange={e => { this.handleInputChange(e.target.value) }} />
-        <p>{this.state.userInput}</p>
+        <input onChange={e => { this.handleChange(e.target.value) }} />
+        <p>{this.state.input}</p>
       </div>
-    );
+    )
   }
-
 }
 
 export default App;
